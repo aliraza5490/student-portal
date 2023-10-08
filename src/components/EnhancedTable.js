3099aefc8752da020ee0941558cc252bcf86fd9c
Ali <ansarialiraza193@gmail.com>
2023-10-08 07:23:39 +0500

    add student list table

--- src/components/EnhancedTable.js
+++ src/components/EnhancedTable.js
@@ -22,31 +22,18 @@ import { visuallyHidden } from "@mui/utils";
 import PropTypes from "prop-types";
 import * as React from "react";
 
-function createData(name, calories, fat, carbs, protein) {
+function createData(name, email, dob, gender, entry_age, gpa) {
   return {
     name,
-    calories,
-    fat,
-    carbs,
-    protein,
+    email,
+    dob,
+    gender,
+    entry_age,
+    gpa,
   };
 }
 
-const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
-];
+const rows = [createData("John", "jdoe@example.com", "01/1990", "M", 18, 3.5)];
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -85,31 +72,37 @@ const headCells = [
     id: "name",
     numeric: false,
     disablePadding: true,
-    label: "Dessert (100g serving)",
+    label: "Name",
   },
   {
-    id: "calories",
-    numeric: true,
+    id: "email",
+    numeric: false,
     disablePadding: false,
-    label: "Calories",
+    label: "Email",
   },
   {
-    id: "fat",
-    numeric: true,
+    id: "dob",
+    numeric: false,
     disablePadding: false,
-    label: "Fat (g)",
+    label: "Date of Birth",
   },
   {
-    id: "carbs",
-    numeric: true,
+    id: "gender",
+    numeric: false,
     disablePadding: false,
-    label: "Carbs (g)",
+    label: "Gender",
   },
   {
-    id: "protein",
+    id: "entry_age",
     numeric: true,
     disablePadding: false,
-    label: "Protein (g)",
+    label: "Entry Age",
+  },
+  {
+    id: "gpa",
+    numeric: true,
+    disablePadding: false,
+    label: "GPA",
   },
 ];
 
@@ -243,7 +236,7 @@ EnhancedTableToolbar.propTypes = {
 
 export default function EnhancedTable() {
   const [order, setOrder] = React.useState("asc");
-  const [orderBy, setOrderBy] = React.useState("calories");
+  const [orderBy, setOrderBy] = React.useState("name");
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
@@ -364,10 +357,11 @@ export default function EnhancedTable() {
                     >
                       {row.name}
                     </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                    <TableCell>{row.email}</TableCell>
+                    <TableCell>{row.dob}</TableCell>
+                    <TableCell>{row.gender}</TableCell>
+                    <TableCell align="right">{row.entry_age}</TableCell>
+                    <TableCell align="right">{row.gpa}</TableCell>
 
                     <TableCell>
                       <IconButton>
