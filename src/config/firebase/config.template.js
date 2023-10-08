f09f6c856e56fe973346cd5da0a7cb385d86ecdd
Ali <ansarialiraza193@gmail.com>
2023-10-08 10:24:43 +0500

    add config template file

---
+++ src/config/firebase/config.template.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  apiKey: "",
+
+  authDomain: "",
+
+  databaseURL: "",
+
+  projectId: "",
+
+  storageBucket: "",
+
+  messagingSenderId: "",
+
+  appId: "",
+};
