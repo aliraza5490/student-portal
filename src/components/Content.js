52859f89a313872d256f1d0f91641ff35533f881
Ali <ansarialiraza193@gmail.com>
2023-10-08 07:01:43 +0500

    init

---
+++ src/components/Content.js
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import EnhancedTable from "./EnhancedTable";
+
+export default function Content() {
+  return (
+    <Box sx={{ width: "100%", margin: "auto" }}>
+      {students.length === 0 && (
+        <Typography sx={{ my: 5, mx: 2 }} color="text.secondary" align="center">
+          No users for this project yet
+        </Typography>
+      )}
+      <EnhancedTable />
+    </Box>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import theme from "@/config/theme";
+import "@/styles/globals.css";
+import { ThemeProvider } from "@mui/material";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
