e27ea7e35f2fae7951a297596c8dd245f7b50f55
Ali <ansarialiraza193@gmail.com>
2023-10-08 13:10:03 +0500

    update config

---
+++ src/config/firebase/config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDPnvIwMbZlT2M3kd_lvn8-0zPqSKKRjrA",
+
+  authDomain: "student-portal-ea12a.firebaseapp.com",
+
+  databaseURL: "https://student-portal-ea12a-default-rtdb.firebaseio.com",
+
+  projectId: "student-portal-ea12a",
+
+  storageBucket: "student-portal-ea12a.appspot.com",
+
+  messagingSenderId: "1026126196025",
+
+  appId: "1:1026126196025:web:37122e55ee38a6bc154ce8",
+};
